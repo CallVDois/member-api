4fe8563de1de81b1571cc949f9fe49c25c2040fe
jhonatapers <jhonatapers@gmail.com>
2025-06-04 14:07:27 -0300

    feat: update DefaultCreateMemberUseCase to use ValidationException and add unit tests for CreateMemberInput and DefaultCreateMemberUseCase

--- application/src/main/java/com/callv2/member/application/member/create/DefaultCreateMemberUseCase.java
+++ application/src/main/java/com/callv2/member/application/member/create/DefaultCreateMemberUseCase.java
@@ -3,7 +3,7 @@ package com.callv2.member.application.member.create;
 import java.util.Objects;
 
 import com.callv2.member.domain.event.EventDispatcher;
-import com.callv2.member.domain.exception.DomainException;
+import com.callv2.member.domain.exception.ValidationException;
 import com.callv2.member.domain.member.entity.Member;
 import com.callv2.member.domain.member.gateway.MemberGateway;
 import com.callv2.member.domain.member.valueobject.Email;
@@ -27,13 +27,13 @@ public class DefaultCreateMemberUseCase extends CreateMemberUseCase {
     }
 
     @Override
-    public CreateMemberOutput execute(final CreateMemberInput anIn) {
+    public CreateMemberOutput execute(final CreateMemberInput input) {
 
-        final Username username = Username.of(anIn.username());
-        final Nickname nickname = Nickname.of(anIn.username());
-        final Email email = Email.of(anIn.email());
+        final Username username = Username.of(input.username());
+        final Nickname nickname = Nickname.of(input.username());
+        final Email email = Email.of(input.email());
 
-        final Password password = Password.of(anIn.password());
+        final Password password = Password.of(input.password());
 
         final PreMember preMember = PreMember.with(username, email, password);
         final Notification notification = Notification.create();
@@ -42,7 +42,7 @@ public class DefaultCreateMemberUseCase extends CreateMemberUseCase {
         preMember.validate(notification);
 
         if (notification.hasError())
-            throw DomainException.with(notification.getErrors());
+            throw ValidationException.with("Validation error", notification.getErrors());
 
         final Member member = this.memberGateway.create(preMember);
         this.eventDispatcher.notify(member);
