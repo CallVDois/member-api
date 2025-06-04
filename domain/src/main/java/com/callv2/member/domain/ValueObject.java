af684faacd3b6e070f7e34dbeea6e5452c6263a9
jhonatapers <jhonatapers@gmail.com>
2025-06-04 18:46:31 -0300

    feat: implement Validatable interface for Entity and ValueObject to enforce validation

--- domain/src/main/java/com/callv2/member/domain/ValueObject.java
+++ domain/src/main/java/com/callv2/member/domain/ValueObject.java
@@ -2,8 +2,9 @@ package com.callv2.member.domain;
 
 import com.callv2.member.domain.validation.ValidationHandler;
 
-public interface ValueObject {
+public interface ValueObject extends Validatable {
 
+    @Override
     default void validate(ValidationHandler aHandler) {
     };
 
