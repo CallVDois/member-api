f4c2fac15308276a3a161423481b5216fbc414ac
jhonatapers <jhonatapers@gmail.com>
2025-06-04 19:16:10 -0300

    fix: remove unused import for RequestBody in MemberAdminAPI

--- infrastructure/src/main/java/com/callv2/member/infrastructure/api/MemberAdminAPI.java
+++ infrastructure/src/main/java/com/callv2/member/infrastructure/api/MemberAdminAPI.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,7 +22,6 @@ import com.callv2.member.infrastructure.member.model.UpdateMemberSystemsRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
