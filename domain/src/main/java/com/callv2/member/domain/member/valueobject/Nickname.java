758dd353e9ed3161d143980fc30df684eb8b71bd
JohnTTA <ttaoficial@gmail.com>
2025-10-05 14:48:40 -0300

    -Refactor on validation: Early return and constant for regex; -Added changeNickname(...) for Member;

--- domain/src/main/java/com/callv2/member/domain/member/valueobject/Nickname.java
+++ domain/src/main/java/com/callv2/member/domain/member/valueobject/Nickname.java
@@ -6,6 +6,8 @@ import com.callv2.member.domain.validation.ValidationHandler;
 
 public record Nickname(String value) implements ValueObject {
 
+    private static final String NICKNAME_PATTERN = "^(?![._])(?!.*[._]{2})[A-Za-z0-9._]{3,20}$";
+
     public static Nickname of(final String nickname) {
         return new Nickname(nickname);
     }
@@ -14,9 +16,10 @@ public record Nickname(String value) implements ValueObject {
     public void validate(ValidationHandler aHandler) {
         if (value == null || value.isBlank()) {
             aHandler.append(Error.with("'nickname' is required"));
-        } else if (!value.matches("^(?![._])(?!.*[._]{2})[A-Za-z0-9._]{3,20}$")) {
-            aHandler.append(Error.with("'nickname' must be 3 to 20 characters long, only letters, digits, underscores, dots, and cannot start/end with or have consecutive special characters."));
+            return;
         }
+        if (!value.matches(NICKNAME_PATTERN)) 
+            aHandler.append(Error.with("'nickname' must be 3 to 20 characters long, only letters, digits, underscores, dots, and cannot start/end with or have consecutive special characters."));
 
     }
 
